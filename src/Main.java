7e09ab59051b7f19c1e0fbdd0381746af1bbf503
tillreichardt <74988736+tillreichardt@users.noreply.github.com>
2023-09-05 18:47:50 +0200

    fibonacci
    calculate fibonacci number a

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println(cal(5));
+        System.out.println(fibonacci(9));
     }
     public static long cal(int n) {
         if (n == 0) {
@@ -10,5 +11,14 @@ public class Main {
         } else {
             return n * cal(n - 1);
         }
+    }
+    private static int fibonacci(int a){
+
+        if (a==1||a==2) {
+            return 1;
+        } else {
+            return fibonacci(a-1)+fibonacci(a-2);
+        }
+
     }
 }
